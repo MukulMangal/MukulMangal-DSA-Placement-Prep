5ff89f91c418f265225a560cd2a5de029608fadf
Mukul Mangal <mangalmukul22@gmail.com>
2026-07-20 21:50:01 +0530

    Time: 2 ms (16.6%), Space: 9.1 MB (96.19%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum  = 0 , diff = 0;
+        for(char c : t){
+            sum += c - 'a';
+        }
+        for(char c : s){
+            diff +=c - 'a';
+        }
+
+        return char(sum - diff + 'a');
+    }
+};
