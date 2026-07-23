229d74cd09025eb19d1ff27c6a5b24687e84ec55
Mukul Mangal <mangalmukul22@gmail.com>
2026-07-23 22:30:57 +0530

    Time: 10 ms (93.08%), Space: 45.9 MB (5.79%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+
+        int i = 0;
+        int j = people.size()-1;
+        int minBoat = 0;
+
+        sort(people.begin() , people.end());
+
+        while(i<=j){
+            if(people[i]+people[j] <= limit){
+                i++;
+
+                minBoat++;
+                j--;
+            }
+            else{
+                j--;
+                minBoat++;
+            }
+        }
+        return minBoat;
+    }
+};
